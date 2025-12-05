82ba46991ddc27b4988d9aab6cb664abe000cb75
capir.2024116isinf0018 <capir.2024116isinf0018@aluno.ifpi.edu.br>
2025-12-05 17:08:52 +0000

    coomiror

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 # Lista de cursos (exemplo)
 cursos = [
-    {"nome": "Astronomia para curiosos", "descricao": "Entenda nosso incrível universo."},
-    {"nome": "Empreendedor mirim", "descricao": "Aprenda a empreender de forma simples"},
-    {"nome": "Criação de jogos", "descricao": "Crie jogos e aprenda a programar."}
+    { "id" : 1,
+    "nome": "Astronomia para curiosos",
+     "descricao": "Entenda nosso incrível universo."
+     },
+    { "id" : 2,
+    "nome": "Empreendedor mirim",
+     "descricao": "Aprenda a empreender de forma simples"
+     },
+    { "id" : 3,
+    "nome": "Criação de jogos",
+     "descricao": "Crie jogos e aprenda a programar."
+     }
 ]
 
 @app.route("/")
 def index():
     return render_template("index.html", cursos=cursos)
 
-@app.route("/curso")
-def curso():
-    return render_template("curso.html")
 
 @app.route("/contato")
 def contato():
     return render_template("contact.html")
+
+@app.route("/curso/<int:id>")
+def curso():
+    return render_template("curso.html")
+
+    curso_selecionado = None
+    
+    for c in cursos:
+        if c.get('id') == id:
+            curso_selecionado = c
+            break
+    if curso_selecionado:
+        return render_template("curso.html", curso=curso_selecionado)
+    else:
+        return "Curso não encontrado", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
