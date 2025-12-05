9d1c8602c7b5ed5674ec68a569fffe444c4b6cb0
capir.2024116isinf0018 <capir.2024116isinf0018@aluno.ifpi.edu.br>
2025-12-05 16:13:25 +0000

    mais

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Lista de cursos (exemplo)
+cursos = [
+    {"nome": "Astronomia para curiosos", "descricao": "Entenda nosso incrível universo."},
+    {"nome": "Empreendedor mirim", "descricao": "Aprenda a empreender de forma simples"},
+    {"nome": "Criação de jogos", "descricao": "Crie jogos e aprenda a programar."}
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", cursos=cursos)
 
 @app.route("/curso")
 def curso():
@@ -15,4 +22,5 @@ def contato():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+
